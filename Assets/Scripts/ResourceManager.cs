ac0ba8bc34abe73ca74fa5bccbd91dae2e62d31f
Dragonayzer <dragonayzer@mail.ru>
2025-06-11 05:53:22 +0300

    Guard clause

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -14,9 +14,9 @@ public class ResourceManager : MonoBehaviour
     private Vector2 worldMax;
 
     void Update() {
-        if (lastResourceSpawn + Mathf.Max(Settings.resourceSpawnRate, resourceSpawnRateCap) < Time.time) {
-            GetComponent<Spawner>().Spawn(worldMin, worldMax);
-            lastResourceSpawn = Time.time;
-        }
+        if (lastResourceSpawn + Mathf.Max(Settings.resourceSpawnRate, resourceSpawnRateCap) > Time.time) { return; }
+        
+        GetComponent<Spawner>().Spawn(worldMin, worldMax);
+        lastResourceSpawn = Time.time;
     }
 }
